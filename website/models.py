78e1149cb8c956e03e53aee13814046b272f8102
Ruan <oliveiraruan2018@gmail.com>
2025-12-04 21:37:36 -0400

    feat: adjusting in modeling model Daily

--- website/models.py
+++ website/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Unit(models.Model):
-    name = models.CharField(max_length=50)
-    code = models.CharField(max_length=50, unique=True, help_text='Código identificador na API de origem')
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True, help_text='Código identificador na API de origem')
     acronym = models.CharField(max_length=25, blank=True, null=True)
 
     def __str__(self):
@@ -13,25 +13,24 @@ class Daily(models.Model):
     code_api = models.CharField(max_length=50, unique=True)
 
     recipient_name = models.CharField(max_length=100)
-    recipient_cpf = models.CharField(max_length=50, blank=True, null=True)
+    recipient_cpf = models.CharField(max_length=20, blank=True, null=True)
+    position  = models.CharField(max_length=50, blank=True, null=True)
 
-    date_payment = models.DateField(help_text="Date de referẽncia para o filtro 'Por data'")
+    date_inicied = models.DateField()
+    date_return = models.DateField()
     value = models.DecimalField(decimal_places=2, max_digits=12, help_text="Valor total da diaria")
 
     reason = models.TextField(blank=True, null=True)
-    destination = models.TextField(blank=True, null=True)
+    destination = models.CharField(max_length=255, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = "Daily"
-        verbose_name_plural = "Dailies"
-        ordering = ['-date_payment']
         indexes = [
-            models.Index(fields=['date_payment']),
+            models.Index(fields=['date_inicied']),
             models.Index(fields=['unit']),
         ]
 
     def __str__(self):
-        return f"{self.date_payment} - {self.recipient_name} - {self.value}"
+        return f"{self.date_inicied} - {self.recipient_name} - {self.value}"
