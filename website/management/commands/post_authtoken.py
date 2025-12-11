4246e91263893242cb0d76378e9f2f5d24494474
Ruan <oliveiraruan2018@gmail.com>
2025-12-10 22:20:07 -0400

    feat: add variables environment

--- website/management/commands/post_authtoken.py
+++ website/management/commands/post_authtoken.py
@@ -9,7 +9,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Posting auth token...")
 
+        api_username = os.environ.get("API_USERNAME")
+        api_password = os.environ.get("API_PASSWORD")
         url = "https://api2.transparencia.rr.gov.br/transparencia/oauth/token?grant_type=client_credentials"
-        response = requests.post(url, auth=("integracao_geral", "agtTAG57732#@#"))
+        response = requests.post(url, auth=(api_username, api_password))
         body = response.json()
         print(body["access_token"])
